f8bb526368509ac8d0544e88df4f8174f04514b1
Mariano Eiberman <mariano.eiberman@mediamonks.com>
2019-03-27 10:11:55 -0300

    add soem comments

--- __tests__/App.js
+++ __tests__/App.js
@@ -2,25 +2,18 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-
+// this will be use the setup test, added from package.json jest config
 import 'react-native';
 import 'jest-enzyme';
-import React from 'react';
 import { View } from 'react-native';
 import { shallow } from 'enzyme';
 
 import App from '../App';
 
-
-class Component1 extends React.Component<{}> {
-  render(){
-    return <div>Marcos</div>;
-  }
-}
-
+// mocking some libraries what are not important to the test case
 jest.mock('@terrylinla/react-native-sketch-canvas', () =>  'Component1');
 
-
+// testing with snapshot testing
 it('renders correctly', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(View)).toMatchSnapshot();
--- setup-tests.js
+++ setup-tests.js
@@ -1,15 +1,15 @@
 import 'react-native';
 import 'jest-enzyme';
-// Note: test renderer must be required after react-native.
-//import renderer from 'react-test-renderer';
+// add Enzyme for test environment
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+// Enzyme uses JSDOM for virtual DOM
 const { JSDOM } = require('jsdom');
-
+// default config for DOM
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const { window } = jsdom;
 
+// Im not using this fn, but its for apply all properties of one object to another.
 function copyProps(src, target) {
     Object.defineProperties(target, {
         ...Object.getOwnPropertyDescriptors(src),
@@ -17,14 +17,17 @@ function copyProps(src, target) {
     });
 }
 
+// setting globals
 global.window = window;
 global.document = window.document;
 global.navigator = {
     userAgent: 'node.js',
 };
 //copyProps(window, global);
-
+//Configuring adapter for react 16
 Enzyme.configure({ adapter: new Adapter() });
+
+// Removing warnings from console, for a cleanest console log
 const originalConsoleError = console.error;
 console.error = (message) => {
     if (message.startsWith('Warning:')) {
